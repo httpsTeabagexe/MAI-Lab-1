c1381d4e9f6e3ede7ba372fa8e6e6b5069a3f3fb
Tim <timurmenshin1@gmail.com>
2022-09-08 21:05:09 +0300

    second rwy

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "Header.h"
+
+//todo вторая впп и ввыыводить в файл на какую впп приземлился летательный аппарат
 int main() {
-	// setlocale(LC_ALL, "Ru");
-	float x = 0, y = 0, length = 3000;
-	float t0 = 1, tk = 5000;
-	TAirport DEMO_AIRPORT(x, y, length);
-	DEMO_AIRPORT.Do(t0, tk);
-	return 0;
+	float x = 0, y = 0, length = 4000;
+	float t0 = 1, tk = 7500;
+	float XY_offset = 1000;
+	float l_offset = 1000;
+	TAirport DEMO(x, y, length); //first airport
+	TAirport DEMO2(x+XY_offset, y+XY_offset, length); //second airport
+	DEMO.Do(t0, tk);
+	DEMO2.Do(t0, tk);
+
+
 }
