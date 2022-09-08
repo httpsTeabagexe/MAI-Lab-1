af096649db1a226256731319369e4c035630a98f
Tim <timurmenshin1@gmail.com>
2022-09-08 21:06:56 +0300

    second rwy

--- Header.h
+++ Header.h
@@ -3,10 +3,19 @@
 #include <iomanip>
 #include <cmath>
 #include <string>
+#include <vector>
 #define PI 3.14159265
-const float dt = 0.01;
-const short num = 20;
+const float dt = 0.1;
+const short num = 40;
+// const short num1 = rand()%num;                   //LA for 1st runway
+// const short num2 = num - num1;           //LA for 2nd runway
+const short RW_count;
 using namespace std;
+
+//tla** la //массив для хранения динамических переменных, 
+    //чтобы можно было удалять не только массив, но и его объекты
+
+//предварительные объявления класса/проттоипы классаё
 class TAObject {
 public:
     float x, y;
@@ -18,7 +27,7 @@ public:
 
 class TLA : public TAObject {
 public:
-    int a, b;
+    int isLanding, b;
     float xc, yc, r, fi, V;
     bool landed,type;
     TLA(float x, float y, float V, float xc, float yc);
@@ -45,5 +54,5 @@ public:
     TLA** LA;
     TAirport(float x, float y, float length);
     void Do(float t0, float tk);
-    //    ~TAirport();
 };
+
--- Source.cpp
+++ Source.cpp
@@ -3,25 +3,26 @@ TAObject::TAObject(float x, float y) {
     this->x = x;
     this->y = y;
 }
-TAObject::~TAObject(){};
+TAObject::~TAObject(){}
+
 TLA::TLA(float x, float y, float V, float xc, float yc)
     : TAObject(x, y) {
     this->xc = xc;
     this->yc = yc;
     this->V = V;
     landed = false;
-    this->a = 0;
+    this->isLanding = 0;
 }
-void TLA::move(float t, int a) {
-    a==0?isBusy=false:isBusy=true;
-    // if (a == 0)  isBusy = false; 
-    // if (a == 1)  isBusy = true; 
+void TLA::move(float t, int isLanding) {
+    isLanding==0?isBusy=false:isBusy=true;
     if (x > xc) b = 0;
     if (x <= xc && y <= yc) { b = -1; }
     if (x <= xc && y >= yc) { b = 1; }
     fi = b * PI + atan((y - yc) / (x - xc));
     r = sqrt(pow((x - xc), 2) + pow((y - yc), 2));
 }
+
+
 TAircraft::TAircraft(float x, float y, float V, float xc, float yc) 
     : TLA(x, y, V, xc, yc) {
     type = 1;
@@ -30,23 +31,27 @@ THelicopter::THelicopter(float x, float  y, float V, float xc, float yc)
     : TLA(x, y, V, xc, yc) {
     type = 0;
 }
-void TAircraft::move(float t, int a) {
-    this->TLA::move(t, this->a);
+
+void TAircraft::move(float t, int isLanding) {
+    this->TLA::move(t, this->isLanding);
     double omega = (-1) * (V / r);
-    xc = -(x + r * cos(fi + omega * t)) * (this->a - 1) + this->a * (xc - V * dt);
-    yc = -(y + r * sin(fi + omega * t)) * (this->a - 1) + yc * this->a;
+    xc = -(x + r * cos(fi + omega * t)) * (this->isLanding - 1) + this->isLanding * (xc - V * dt);
+    yc = -(y + r * sin(fi + omega * t)) * (this->isLanding - 1) + yc * this->isLanding;
 }
-void THelicopter::move(float t, int a) {
-    this->TLA::move(t, this->a);
-    xc = xc + (this->a * V * cos(fi) * dt);
-    yc = yc + (this->a * V * sin(fi) * dt);
+void THelicopter::move(float t, int isLanding) {
+    this->TLA::move(t, this->isLanding);
+    xc = xc + (this->isLanding * V * cos(fi) * dt);
+    yc = yc + (this->isLanding * V * sin(fi) * dt);
 }
+
+TAirport::TAirport(){};
+
 TAirport::TAirport(float x, float y, float length)
     : TAObject(x, y) {
+        srand(time(NULL));
     this->length = length;
     LA = new TLA* [num];    
     float xc, yc, V;
-    srand(time(NULL));
     for (int i = 0; i < num; i++) {
 
         xc = rand() % 10000 + 10000;
@@ -57,42 +62,62 @@ TAirport::TAirport(float x, float y, float length)
         cout << "Veloc. V = " << V << endl;
         cout << "_________________" << endl;
         bool type = rand() % 2;
-        type? LA[i] = new TAircraft(x, y, V, xc, yc): LA[i] = new THelicopter(x, y, V, xc, yc);
+        type? LA[i] = new TAircraft(x, y, V, xc, yc) 
+        : LA[i] = new THelicopter(x, y, V, xc, yc);
     }
 }
-void TAirport::Do(float t0, float tk) {
-    ofstream FILE("LOG.txt");
-    for (float time = t0; time <= tk; time += dt) {
-        for (int i = 0; i < num; i++) {
-            if (LA[i]->landed)
+void TAirport::Do(float t0, float tk){
+    short rw1=1; short rw2=2;
+    ofstream FILE1("LOG1.txt"); ofstream FILE2("LOG2.txt");
+
+
+    TAirport* AP = new TAirport[RW_count];    
+    //if(AP[0].isBusy)
+for(int i = 0; i<RW_count ; i++){
+
+    for(float time = t0; time<tk;time+=dt){
+        if (LA[i]->landed)
                 continue;
             double X = this->x + 1.1 * this->length;
             if (LA[i]->type) {
-                if (!LA[i]->a) {
-                    if ((!(this->isBusy && !LA[i]->isBusy)) && (LA[i]->xc > X) && (abs(LA[i]->yc - this->y) < (this->length / 50))) {
-                        LA[i]->a = 1;
-                        this->isBusy = true;
+                if (!LA[i]->isLanding) {
+                    if ((!(AP[i].isBusy && !LA[i]->isBusy)) && (LA[i]->xc > X) && (abs(LA[i]->yc - this->y) < (this->length / 50))) {
+                        LA[i]->isLanding = 1;
+                        
+                        AP[i].isBusy = true;
                     }
-                    else LA[i]->a = 0;
+                    else LA[i]->isLanding = 0;
                 }
                 else {
                     if ((LA[i]->xc < (this->x + this->length)) && LA[i]->isBusy) {
                         LA[i]->landed = true;
                         index++;
-                        this->isBusy = false;
+                        AP[i].isBusy = false;
                     }
                     else
-                    if (this->isBusy) LA[i]->landed = false;
+                    if (AP[i].isBusy)
+                        LA[i]->landed = false;
                 }
             }
+    }
+    }
+}
+/*
+void TAirport::Do(float t0, float tk) {
+    short rwy1 = 1; short rwy2 = 2;
+    ofstream FILE("LOG1.txt");
+    ofstream FILE2("LOG2.txt");
+    for (float time = t0; time <= tk; time += dt) {
+        for (int i = 0; i < num1; i++) {
+            
             else {
-                if (!LA[i]->a) {
+                if (!LA[i]->isLanding) {
                     if (!(this->isBusy && !(LA[i]->isBusy))) {
-                        LA[i]->a = 1;
+                        LA[i]->isLanding = 1;
                         this->isBusy = true;
                     }
                     else {
-                        LA[i]->a = 0;
+                        LA[i]->isLanding = 0;
                     }
                 }
                 else {
@@ -106,15 +131,18 @@ void TAirport::Do(float t0, float tk) {
                     }
                 }
             }
-            LA[i]->move(time, LA[i]->a);
+            LA[i]->move(time, LA[i]->isLanding);
             //output to file aircraft type and number, time of landing
             if (LA[i]->landed) {
-                if (LA[i]->type) 
-                FILE << "Aircraft " << i << " landed at " << time << " sec." << endl;
+                if (LA[i]->type)
+                    FILE << "Aircraft " << i << " landed at " << time << " sec." << "at the runway №: " << rwy << endl;
                 else
-                FILE << "Helicopter " << i << " landed at " << time << " sec." << endl;
+                    FILE << "Helicopter " << i << " landed at " << time << " sec." << "at the runway №: "<< rwy << endl;
             }
+
         }
     }
     FILE.close();
 }
+*/
+
